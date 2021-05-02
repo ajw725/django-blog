764452c97718e1d037ee8c59018ff292ecf81802
Andrew Weinstein <andrew@altvia.com>
2021-05-02 09:25:51 -0600

    set up post and comment models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    published_at = models.DateTimeField(blank=True, null=True)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def publish(self):
+        self.published_at = timezone.now()
+        self.save()
+
+    def approved_comments(self):
+        return self.comments.filter(approved=True)
+
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    body = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    approved = models.BooleanField(default=False)
+    author = models.CharField(max_length=255)
+    post = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE)
+
+    def approve(self):
+        self.approved = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('post_list')
+
+    def __str__(self):
+        return self.body
