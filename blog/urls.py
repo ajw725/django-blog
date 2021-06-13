87bf23868c9cab11fb10f3f40b597fd2f5922c5e
Andrew Weinstein <aweinstein7@gmail.com>
2021-06-13 15:37:52 -0600

    add comment views and empty HTML templates

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('posts/new/', views.PostCreateView.as_view(), name='post_new'),
     path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_update'),
     path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
-    path('posts/drafts/', views.DraftListView.as_view(), name='post_drafts')
+    path('posts/drafts/', views.DraftListView.as_view(), name='post_drafts'),
+    path('posts/<int:pk>/publish/', views.publish_post, name='publish_post'),
+    path('posts/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comments/<int:pk>/approve/', views.approve_comment, name='approve_comment'),
+    path('comments/<int:pk>/remove/', views.remove_comment, name='remove_comment')
 ]
